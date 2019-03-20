6464eb6a84902ca469c627dc83e20b39fb4bfa45
Marco La Rosa <m@lr.id.au>
2019-03-20 13:00:38 +1100

    restructure routes

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ Vue.use(VueRouter);
 import ShellComponent from "components/Shell.component.vue";
 import BadRequestComponent from "components/BadRequest.component.vue";
 import ViewImageComponent from "components/ViewImage.component.vue";
+import RenderListComponent from "components/RenderList.component.vue";
 
 export const router = new VueRouter({
     mode: "history",
@@ -15,12 +16,19 @@ export const router = new VueRouter({
         {
             path: "/",
             name: "shell",
-            component: ShellComponent
-        },
-        {
-            path: "/view-image",
-            name: "viewImage",
-            component: ViewImageComponent
+            component: ShellComponent,
+            children: [
+                {
+                    path: "",
+                    name: "viewList",
+                    component: RenderListComponent
+                },
+                {
+                    path: "view-image",
+                    name: "viewImage",
+                    component: ViewImageComponent
+                }
+            ]
         }
     ]
 });
