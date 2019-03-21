3d3c90cba56ebfb2600a83e76b0eb41dae058b7d
Marco La Rosa <m@lr.id.au>
2019-03-21 14:32:46 +1100

    change dev server port

--- webpack.develop.js
+++ webpack.develop.js
@@ -36,7 +36,7 @@ module.exports = {
         contentBase: path.join(__dirname, "dist"),
         compress: true,
         host: "0.0.0.0",
-        port: 9000,
+        port: 9001,
         historyApiFallback: true,
         watchContentBase: true
     },
