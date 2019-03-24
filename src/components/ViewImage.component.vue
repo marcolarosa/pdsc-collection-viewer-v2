43b24bd74702c452ef53ba22618dbf6eba93ea0d
Marco La Rosa <m@lr.id.au>
2019-03-25 09:17:25 +1100

    fix the image top left

--- src/components/ViewImage.component.vue
+++ src/components/ViewImage.component.vue
@@ -1,5 +1,7 @@
 <template>
-    <img :src="image">
+    <div class="style-image-container">
+        <img :src="image" class="style-image">
+    </div>
 </template>
 
 <script>
@@ -16,6 +18,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.style-image-container {
+    position: fixed;
+    overflow: scroll;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+}
+
+.style-image {
+}
 </style>
 
 
