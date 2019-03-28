d51ddad88be2ca3433b14a768e3a3bb9fba9cdf6
Marco La Rosa <m@lr.id.au>
2019-03-28 16:00:18 +1100

    implement image nav controls

--- src/components/ViewImage.component.vue
+++ src/components/ViewImage.component.vue
@@ -1,30 +1,93 @@
 <template>
-    <div class="style-image-container">
-        <img :src="image.item.path" class="style-image" v-if="image.item">
+    <div>
+        <div class="style-image-container" @click="toggleControls">
+            <img :src="image.item.path" class="style-image" v-if="image.item">
+        </div>
+
+        <view-image-controls
+            v-if="dialogVisible"
+            :image="image"
+            v-on:toggle-controls="toggleControls"
+            v-on:previous-image="goToPreviousImage"
+            v-on:image-list="goToImageList"
+            v-on:next-image="goToNextImage"
+        />
     </div>
 </template>
 
 <script>
+import { findIndex } from "lodash";
+import ViewImageControls from "./ViewImageControls.component.vue";
+
 export default {
+    components: {
+        ViewImageControls
+    },
     data() {
         return {
-            image: {}
+            image: {},
+            images: [],
+            dialogVisible: false
         };
     },
     mounted() {
-        let { collectionId, itemId, image } = this.$route.params;
-        if (!this.$store.state.list.length) {
-            this.$router.push({ name: "viewList" });
-            return;
+        this.loadImage();
+    },
+    watch: {
+        $route(to, from) {
+            this.loadImage();
         }
-        let images = this.$store.state.list.filter(item => {
-            return item.collectionId === collectionId && item.itemId === itemId;
-        })[0].images;
-        if (!images.length) {
-            this.$router.push({ name: "viewList" });
-            return;
+    },
+    methods: {
+        loadImage() {
+            let { collectionId, itemId, image } = this.$route.params;
+            if (!this.$store.state.list.length) {
+                this.$router.push({ name: "viewList" });
+                return;
+            }
+            this.images = this.$store.state.list.filter(item => {
+                return (
+                    item.collectionId === collectionId && item.itemId === itemId
+                );
+            })[0].images;
+            if (!this.images.length) {
+                this.$router.push({ name: "viewList" });
+                return;
+            }
+            this.image = this.images.filter(i => i.name.match(image))[0];
+        },
+        toggleControls() {
+            this.dialogVisible = !this.dialogVisible;
+        },
+        goToPreviousImage() {
+            let itemIndex = findIndex(this.images, { name: this.image.name });
+            if (itemIndex > 0) {
+                let previous = this.images[itemIndex - 1];
+                this.$router.push({
+                    path: `/image/${this.image.collectionId}/${
+                        this.image.itemId
+                    }/${previous.name.split(".")[0]}`
+                });
+            }
+            this.loadImage();
+        },
+        goToNextImage() {
+            let itemIndex = findIndex(this.images, { name: this.image.name });
+            if (itemIndex < this.images.length - 1) {
+                let next = this.images[itemIndex + 1];
+                this.$router.push({
+                    path: `/image/${this.image.collectionId}/${
+                        this.image.itemId
+                    }/${next.name.split(".")[0]}`
+                });
+            }
+            this.loadImage();
+        },
+        goToImageList() {
+            this.$router.push({
+                path: `/images/${this.image.collectionId}/${this.image.itemId}`
+            });
         }
-        this.image = images.filter(i => i.name.match(image))[0];
     }
 };
 </script>
@@ -40,6 +103,7 @@ export default {
 }
 
 .style-image {
+    width: 100%;
 }
 </style>
 
---
+++ src/components/ViewImageControls.component.vue
@@ -0,0 +1,99 @@
+<template>
+    <div>
+        <div class="controls-underlay"></div>
+        <div class="controls-overlay">
+            <div class="row">
+                <div class="col">
+                    <span @click="toggleControls" class="px-5 float-right style-controls">
+                        <i class="fal fa-times"></i>
+                    </span>
+                </div>
+            </div>
+            <div class="row text-center my-4">
+                <div class="col">
+                    <span class="style-title">{{image.title}}</span>
+                    <span class="style-image-name">{{image.name}}</span>
+                </div>
+            </div>
+
+            <div class="row my-5 text-center style-controls">
+                <div class="col-4 my-5 py-5">
+                    <span @click="back">
+                        <i class="fal fa-chevron-circle-left"></i>
+                    </span>
+                </div>
+                <div class="col-4 my-5 py-5">
+                    <span @click="up">
+                        <i class="fal fa-level-up"></i>
+                    </span>
+                </div>
+                <div class="col-4 my-5 py-5">
+                    <span @click="forward">
+                        <i class="fal fa-chevron-circle-right"></i>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        image: Object
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        toggleControls() {
+            this.$emit("toggle-controls");
+        },
+        back() {
+            this.$emit("previous-image");
+        },
+        up() {
+            this.$emit("image-list");
+        },
+        forward() {
+            this.$emit("next-image");
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.controls-underlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    opacity: 0.7;
+    background-color: #000;
+}
+
+.controls-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    color: #fff;
+}
+
+.style-title {
+    display: block;
+    font-size: 3em;
+}
+
+.style-image-name {
+    display: block;
+    font-size: 1.5em;
+}
+
+.style-controls {
+    font-size: 4em;
+}
+</style>
+
