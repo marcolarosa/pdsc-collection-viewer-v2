ea379605fce93319c984e96e9a6b5f8894b284a9
Marco La Rosa <m@lr.id.au>
2019-04-02 14:26:52 +1100

    adjust control size based on screen and center vertically

--- src/components/ViewImageControls.component.vue
+++ src/components/ViewImageControls.component.vue
@@ -9,37 +9,39 @@
                     </span>
                 </div>
             </div>
-            <div class="row text-center my-4">
+            <div class="row text-center">
                 <div class="col">
                     <span class="style-title">{{image.title}}</span>
                     <span class="style-image-name">{{image.name}}</span>
                 </div>
             </div>
 
-            <div class="row my-5 text-center style-controls">
-                <div class="col-3 my-5 py-5">
-                    <span @click="back" class="style-control">
-                        <i class="fal fa-chevron-circle-left"></i>
-                        <span class="style-control-text">previous image</span>
-                    </span>
-                </div>
-                <div class="col-3 my-5 py-5">
-                    <span @click="up" class="style-control">
-                        <i class="fal fa-level-up"></i>
-                        <span class="style-control-text">up to image list</span>
-                    </span>
-                </div>
-                <div class="col-3 my-5 py-5">
-                    <span @click="zoom" class="style-control">
-                        <i class="fal fa-search"></i>
-                        <span class="style-control-text">enable zoom</span>
-                    </span>
-                </div>
-                <div class="col-3 my-5 py-5">
-                    <span @click="forward" class="style-control">
-                        <i class="fal fa-chevron-circle-right"></i>
-                        <span class="style-control-text">next image</span>
-                    </span>
+            <div class="center-controls">
+                <div class="row text-center style-controls mx-auto">
+                    <div class="col-3">
+                        <span @click="back" class="style-control">
+                            <i class="fal fa-chevron-circle-left"></i>
+                            <span class="style-control-text">previous image</span>
+                        </span>
+                    </div>
+                    <div class="col-3">
+                        <span @click="up" class="style-control">
+                            <i class="fal fa-level-up"></i>
+                            <span class="style-control-text">up to image list</span>
+                        </span>
+                    </div>
+                    <div class="col-3">
+                        <span @click="zoom" class="style-control">
+                            <i class="fal fa-search"></i>
+                            <span class="style-control-text">enable zoom</span>
+                        </span>
+                    </div>
+                    <div class="col-3">
+                        <span @click="forward" class="style-control">
+                            <i class="fal fa-chevron-circle-right"></i>
+                            <span class="style-control-text">next image</span>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -94,31 +96,79 @@ export default {
     color: #fff;
 }
 
-.style-title {
-    display: block;
-    font-size: 3em;
-}
-
-.style-image-name {
-    display: block;
-    font-size: 1.5em;
-}
-
-.style-controls {
-    font-size: 4em;
-}
-
 .style-control {
     cursor: pointer;
 }
 
+.center-controls > * {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateY(-50%) translateX(-50%);
+    width: 100vw;
+}
+
+.style-title {
+    display: block;
+    font-size: 1em;
+}
+
+.style-image-name {
+    display: block;
+    font-size: 1em;
+}
+
+.style-controls {
+    font-size: 1.5em;
+}
+
 .style-control-text {
     display: block;
-    font-size: 20px;
+    font-size: 15px;
 }
 
 .style-close {
     font-size: 2.8em;
+}
+@media only screen and (min-width: 600px) and (min-height: 500px) {
+    .style-title {
+        display: block;
+        font-size: 2em;
+    }
+
+    .style-image-name {
+        display: block;
+        font-size: 2em;
+    }
+
+    .style-controls {
+        font-size: 2em;
+    }
+
+    .style-control-text {
+        display: block;
+        font-size: 20px;
+    }
+}
+@media only screen and (min-width: 900px) {
+    .style-title {
+        display: block;
+        font-size: 3em;
+    }
+
+    .style-image-name {
+        display: block;
+        font-size: 1.5em;
+    }
+
+    .style-controls {
+        font-size: 4em;
+    }
+
+    .style-control-text {
+        display: block;
+        font-size: 20px;
+    }
 }
 </style>
 
