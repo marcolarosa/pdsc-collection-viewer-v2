43fd0520609a9c565aa1171008dcfe9519260938
Marco La Rosa <m@lr.id.au>
2019-04-08 15:04:41 +1000

    open media files in fullscreen dialog

--- src/components/RenderAudio.component.vue
+++ src/components/RenderAudio.component.vue
@@ -4,20 +4,45 @@
             <div class="col text-center">{{audio.name}}</div>
         </div>
         <div class="row">
-            <div class="col text-center style-audio-icon">
-                <!-- <router-link :to="{ name: 'viewAudio', params: { audio: audio }}"> -->
+            <div class="col text-center style-audio-icon" @click="dialogVisible = !dialogVisible">
                 <i class="fal fa-volume-up"></i>
-                <!-- </router-link> -->
             </div>
         </div>
-        <div class="row mx-2">
-            <div class="col text-center">
-                <audio class>
-                    <source :src="item" v-for="(item, idx) of audio.item" :key="idx">Your browser does not support the
-                    <code>audio</code> element.
-                </audio>
+
+        <el-dialog
+            :visible.sync="dialogVisible"
+            :fullscreen="true"
+            :before-close="beforeClose"
+            @open="play"
+        >
+            <div class="my-2 text-center">
+                <div class="style-headline text-center style-tile-title">{{audio.title}}</div>
+                <div class="row my-2">
+                    <div class="col style-item-id">
+                        {{audio.collectionId}} /
+                        {{audio.itemId}}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">{{audio.name}}</div>
+                </div>
+                <div class="row my-2">
+                    <div class="col style-audio-icon" @click="dialogVisible = !dialogVisible">
+                        <!-- <router-link :to="{ name: 'viewAudio', params: { audio: audio }}"> -->
+                        <i class="fal fa-volume-up"></i>
+                        <!-- </router-link> -->
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <audio controls autoplay ref="audioElement" class="style-audio-element">
+                            <source :src="item" v-for="(item, idx) of audio.item" :key="idx">Your browser does not support the
+                            <code>audio</code> element.
+                        </audio>
+                    </div>
+                </div>
             </div>
-        </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -27,14 +52,32 @@ export default {
         audio: Object
     },
     data() {
-        return {};
+        return {
+            dialogVisible: false
+        };
+    },
+    methods: {
+        beforeClose() {
+            this.$refs["audioElement"].pause();
+            this.dialogVisible = false;
+        },
+        play() {
+            setTimeout(() => {
+                this.$refs["audioElement"].play();
+            }, 500);
+        }
     }
 };
 </script>
 
 <style lang="scss" scoped>
 .style-audio-icon {
+    cursor: pointer;
     font-size: 6em;
+}
+
+.style-audio-element {
+    width: 90%;
 }
 </style>
 
--- src/components/RenderVideo.component.vue
+++ src/components/RenderVideo.component.vue
@@ -3,16 +3,42 @@
         <div class="row">
             <div class="col text-center">{{video.name}}</div>
         </div>
-        <div class="row">
+        <div class="row" @click="dialogVisible = !dialogVisible">
             <div class="col text-center">
-                <!-- <router-link :to="{ name: 'viewVideo', params: { video: video }}"> -->
-                <video class="style-video mx-auto">
+                <video controls class="style-video mx-auto video-ignore-events">
                     <source :src="item" v-for="(item, idx) of video.item" :key="idx">Your browser does not support the
                     <code>video</code> element.
                 </video>
-                <!-- </router-link> -->
             </div>
         </div>
+
+        <el-dialog
+            :visible.sync="dialogVisible"
+            :fullscreen="true"
+            :before-close="beforeClose"
+            @open="play"
+        >
+            <div class="my-2 text-center">
+                <div class="style-headline text-center style-tile-title">{{video.title}}</div>
+                <div class="row my-2">
+                    <div class="col style-item-id">
+                        {{video.collectionId}} /
+                        {{video.itemId}}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">{{video.name}}</div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <video controls autoplay class="style-video mx-auto" ref="videoElement">
+                            <source :src="item" v-for="(item, idx) of video.item" :key="idx">Your browser does not support the
+                            <code>video</code> element.
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -22,13 +48,31 @@ export default {
         video: Object
     },
     data() {
-        return {};
+        return {
+            dialogVisible: false
+        };
+    },
+    methods: {
+        beforeClose() {
+            this.$refs["videoElement"].pause();
+            this.dialogVisible = false;
+        },
+        play() {
+            setTimeout(() => {
+                this.$refs["videoElement"].play();
+            }, 500);
+        }
     }
 };
 </script>
 
 <style lang="scss" scoped>
+.video-ignore-events {
+    pointer-events: none;
+}
+
 .style-video {
+    cursor: pointer;
     max-width: 90%;
 }
 </style>
