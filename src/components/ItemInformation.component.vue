3f54288f0b4da8d4f0299b06431555d952ff6c28
Marco La Rosa <m@lr.id.au>
2019-04-12 14:17:27 +1000

    implement item information dialog

--- src/components/ItemInformation.component.vue
+++ src/components/ItemInformation.component.vue
@@ -9,46 +9,73 @@
             info
             <i class="fas fa-caret-down"></i>
         </el-button>
-        <el-dialog
-            title="Item Information"
-            :visible.sync="dialogVisible"
-            :fullscreen="true"
-            :modal="false"
-        >
-            <div class="row">
-                <div class="col">{{info.title}}</div>
-            </div>
-            <div class="row">
-                <div class="col">{{info.collectionId}} / {{info.itemId}}</div>
-            </div>
-            <div class="row my-5">
-                <div class="col">What else here?</div>
-            </div>
+        <el-dialog :visible.sync="dialogVisible" :fullscreen="true" :modal="false">
+            <span class="style-item-information">
+                <div class="row py-2">
+                    <div class="col-12 col-md col-lg col-xl style-information-title">Title</div>
+                    <div class="col-12 col-md-10 col-lg-10 col-xl-10">{{item.title}}</div>
+                </div>
+                <div class="row py-2">
+                    <div class="col-12 col-md col-lg col-xl style-information-title">Date</div>
+                    <div class="col-12 col-md-10 col-lg-10 col-xl-10">{{item.date}}</div>
+                </div>
+                <div class="row py-2">
+                    <div class="col-12 col-md col-lg col-xl style-information-title">Metadata</div>
+                    <div
+                        class="col-12 col-md-10 col-lg-10 col-xl-10"
+                    >{{item.collectionId}} / {{item.itemId}}</div>
+                </div>
+                <div class="row py-2" v-if="speakers.length">
+                    <div class="col-12 col-md col-lg col-xl style-information-title">Speakers</div>
+                    <div class="col-12 col-md-10 col-lg-10 col-xl-10">
+                        <div v-for="(speaker, idx) of speakers" :key="idx">{{speaker}}</div>
+                    </div>
+                </div>
+                <div class="row py-2">
+                    <div class="col-12 col-md col-lg col-xl style-information-title">Description</div>
+                    <div class="col-12 col-md-10 col-lg-10 col-xl-10">{{item.description}}</div>
+                </div>
+                <div class="row py-2">
+                    <div class="col-12 col-md col-lg col-xl style-information-title">Citation</div>
+                    <div class="col-12 col-md-10 col-lg-10 col-xl-10">{{item.citation}}</div>
+                </div>
+            </span>
         </el-dialog>
     </span>
 </template>
 
 <script>
-import { loadData, flattenItemList } from "../data-loader.service";
+import { compact } from "lodash";
 
 export default {
     props: {
-        item: Object
+        itemId: String,
+        collectionId: String
     },
     computed: {
-        info: function() {
-            let items = flattenItemList(this.$store.state.items);
-            let item = items.filter(item => {
-                return (
-                    item.collectionId === this.item.collectionId &&
-                    item.itemId === this.item.itemId
-                );
-            })[0];
-            return item;
+        item: function() {
+            return this.$store.getters.item({
+                collectionId: this.collectionId,
+                itemId: this.itemId
+            });
+        },
+        speakers: function() {
+            let speakers = this.item.speakers.map(speaker => {
+                if (this.speakerRolesToDisplay.includes(speaker.role))
+                    return speaker.name;
+            });
+            return compact(speakers);
         }
     },
     data() {
         return {
+            speakerRolesToDisplay: [
+                "participant",
+                "performer",
+                "signer",
+                "singer",
+                "speaker"
+            ],
             dialogVisible: false
         };
     },
@@ -62,6 +89,17 @@ export default {
 .style-information-button {
     color: $background-color-dark;
 }
-</style>
 
+.style-item-information {
+    text-align: left;
+    font-size: 20px;
+    letter-spacing: 1px;
+    color: $background-color-dark;
+}
+
+.style-information-title {
+    font-weight: 500;
+    font-size: 24px;
+}
+</style>
 
