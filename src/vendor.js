5340a76aeba0da4f936b43588fd53d4d58a3d201
Marco La Rosa <m@lr.id.au>
2019-04-12 17:32:35 +1000

    add hammerjs and upgrade imageviewer

--- src/vendor.js
+++ src/vendor.js
@@ -3,10 +3,10 @@
 import "element-ui/lib/theme-chalk/index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/global-styles.scss";
-import "imageviewer/imageviewer.css";
+import "iv-viewer/dist/iv-viewer.css";
 
+import ImageViewer from "iv-viewer";
 import "element-ui/lib/index.js";
 import fontawesome from "@fortawesome/fontawesome-pro/js/all";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoReplaceSvg = "nest";
-import imageviewer from "imageviewer/";
