4361231775f938fb92bda39a2d0451523de32267
Marco La Rosa <m@lr.id.au>
2019-04-12 14:15:53 +1000

    implement getters to retrieve flattened item list and specific item

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@
 
 import Vue from "vue";
 import Vuex from "vuex";
+import { flattenDeep } from "lodash";
 Vue.use(Vuex);
 
 const configuration = {
@@ -27,6 +28,29 @@ const configuration = {
         },
         setSelectedFilter(state, selectedFilter) {
             state.selectedFilter = selectedFilter;
+        }
+    },
+    getters: {
+        itemsFlattened: state => {
+            return flattenDeep(
+                state.items.map(item => {
+                    let components = [];
+                    if (item.images.length) {
+                        components.push({
+                            ...item.images[0],
+                            images: item.images
+                        });
+                    }
+                    return [...components, ...item.audio, ...item.video];
+                })
+            );
+        },
+        item: state => ({ collectionId, itemId }) => {
+            return state.items.filter(item => {
+                return (
+                    item.collectionId === collectionId && item.itemId === itemId
+                );
+            })[0];
         }
     }
 };
