70c1aa53b7971cd6747ed357cdf4143a80a003f1
Marco La Rosa <m@lr.id.au>
2019-04-12 17:33:02 +1000

    load data when shell mounts

--- src/components/Shell.component.vue
+++ src/components/Shell.component.vue
@@ -5,6 +5,32 @@
 </template>
 
 <script>
+import { loadData } from "../data-loader.service";
+import { flattenDeep, includes, orderBy, debounce } from "lodash";
+
+export default {
+    data() {
+        return {};
+    },
+    mounted() {
+        (async () => {
+            this.setItemsToLoad();
+            let { items, filters } = await loadData();
+            this.$store.commit(
+                "setItems",
+                orderBy(items, ["collectionId", "itemId"])
+            );
+            this.$store.commit("setFilters", filters);
+        })();
+    },
+    methods: {
+        setItemsToLoad() {
+            if (window.innerWidth > 800 || window.innerHeight > 800) {
+                this.itemsToPush = 10;
+            }
+        }
+    }
+};
 </script>
 
 <style scoped lang="scss">
